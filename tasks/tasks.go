d4551fe93921a933617ca08bffe4b047c5130f6b
AHADI-DANIEL <daniel@ahadicorp.com>
2023-10-02 11:07:25 +0300

    refactor tasks.go

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -9,54 +9,53 @@ import (
 	"github.com/hibiken/asynq"
 )
 
-//define list of task types
+
+// define list of task types
 const (
 	TypeFluxdbFetch = "fluxdb:fetchrecords"
 )
 
 //define payloads for tasks
 
-//for TypeFluxdbFetch
+// for TypeFluxdbFetch
 type FluxdbFetchPayload struct {
-	BucketName string
+	BucketName        string
 	DestinationBucket string
 }
-
-
 
 //create task
 //task has type and payload
 
-func NewFluxdbFetch(bucketName string, destinationBucket string)(*asynq.Task, error){
+func NewFluxdbFetch(bucketName string, destinationBucket string) (*asynq.Task, error) {
 	payload, err := json.Marshal(FluxdbFetchPayload{BucketName: bucketName, DestinationBucket: destinationBucket})
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-
-	//no error 
+	fmt.Println("Starting tasks...")
+	//no error
 	//return new asynq task
-	return asynq.NewTask(TypeFluxdbFetch,payload,asynq.MaxRetry(5), asynq.Timeout(30 * time.Minute)), nil
-	
+	return asynq.NewTask(TypeFluxdbFetch, payload, asynq.MaxRetry(5), asynq.Timeout(30*time.Minute)), nil
+
 }
 
 //func to handle task xxx
 //satisfies asynq.HandleFunc interface
 //can also be a type that satisfies asynq.Handler interface
 
-//need to accept, ctx and pointer to task
-//need to return error if task failed
-//use HandlerFunc adapter that
-//allows ordinary funcs as handlers
-func HandleFluxdbFetch(ctx context.Context, t *asynq.Task)error{
+// need to accept, ctx and pointer to task
+// need to return error if task failed
+// use HandlerFunc adapter that
+// allows ordinary funcs as handlers
+func HandleFluxdbFetch(ctx context.Context, t *asynq.Task) error {
+	fmt.Println(t.Type())
 	//to store unmarshalled res
 	var p FluxdbFetchPayload
-	if err := json.Unmarshal(t.Payload(), &p); err != nil{
-		return fmt.Errorf("Json failed to unmarshall: %v: %w", err,asynq.SkipRetry)
+	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+		return fmt.Errorf("Json failed to unmarshall: %v: %w", err, asynq.SkipRetry)
 
 	}
+	fmt.Println("Sent task download...")
 	//code to fetch flux records here
 	return nil
 
-
 }
-
